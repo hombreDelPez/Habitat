0c7432b54d6c5fedca9a1b1625a8b8df4046fdf4
Manuel Fischer <manuel.fischer@namics.com>
2018-01-12 14:43:12 +0100

    Registering NitroNet View Engine

---
+++ src/Foundation/Sitecore.Foundation.NitroNet/RegisterNitroNetViewEngine.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using Sitecore.Pipelines;
+using SitecoreNitroNetViewEngine = NitroNet.Sitecore.SitecoreNitroNetViewEngine;
+
+namespace Sitecore.Foundation.NitroNet
+{
+    public class RegisterNitroNetViewEngine
+    {
+        public virtual void Process(PipelineArgs args)
+        {
+            ViewEngines.Engines.Add(DependencyResolver.Current.GetService<SitecoreNitroNetViewEngine>());
+        }
+    }
+}
